b6b9a6deea2e9a943ee0cb1917f95eb8b76bd32a
sarahozeto <127869612+sarahozeto@users.noreply.github.com>
2024-09-11 08:59:17 -0300

    criação do require para incluir controladores

---
+++ routes.php
@@ -0,0 +1,9 @@
+<?php
+//inclui arquivos de controlador nescessários para lidar com diferentes ações 
+require 'controllers/AuthController.php';
+//inclui o controlador de autenticação
+require 'controllers/UserController.php';
+//inclui o controlador de usuários
+require 'controllers/DashboardController.php';
+//inclui o controlador de dashboard
+?>
