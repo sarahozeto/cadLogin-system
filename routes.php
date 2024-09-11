4711fca9c77c908caa85011e65819b2e739ecfae
sarahozeto <127869612+sarahozeto@users.noreply.github.com>
2024-09-11 09:13:13 -0300

    adicionado instância de controladores

--- routes.php
+++ routes.php
@@ -6,4 +6,12 @@ require 'controllers/UserController.php';
 //inclui o controlador de usuários
 require 'controllers/DashboardController.php';
 //inclui o controlador de dashboard
+
+//Cria instâncias dos controladores para utilizar seus métodos
+$authController = new AuthController();
+//instância controlador de auteticação
+$userController = new UserController();
+//instância controlador de usuário
+$dashboardController = new DashboardController();
+//instância controlador de dashboard
 ?>
