25ee8cf92b79cb636782dbc61080c682dc498df9
sarahozeto <127869612+sarahozeto@users.noreply.github.com>
2024-09-11 09:30:26 -0300

    criados função para realizar login

---
+++ controllers/AuthController.php
@@ -0,0 +1,14 @@
+<?php
+//requer o arquivo user que contém o model user com as fubções de manipulaão de dados de usuários
+require_once 'models/user.php';
+
+class AuthController
+{
+    //função responsável responsável para realizar login
+    public function login()
+    {
+        //Verifica se a requisição HTTP é do tipo POST, ou seja, se o formulário foi enviado
+        
+    }
+}
+?>
