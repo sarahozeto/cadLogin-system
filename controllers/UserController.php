0a8a9a1544a3cbfed35dbfa52e0522f0823f7f7e
sarahozeto <127869612+sarahozeto@users.noreply.github.com>
2024-09-17 08:28:06 -0300

    Cria Controlador de Usuário com as funções:sdffsd     - Função de registro (register) que coleta       as informações do formulário de registro       usuário e as armazena em um array.

---
+++ controllers/UserController.php
@@ -0,0 +1,23 @@
+<?php
+class UserController{
+
+    public function register(){
+        // verficia se a requisicão HTTP é do tipo POST (se o formulário foi enviado)
+        if($_SERVER['REQUEST_METHOD'] == 'POST'){
+             // coleta os dados enviados pelo formulário e organiza em uma array
+             $data = [
+                'nome' => $_POST['nome'],
+                'email' => $_POST['email'],
+                'senha' => password_hash($_POST['senha'], PASSWORD_DEFAULT), // criptografa a senha
+                'perfil' => $_POST['perfil']
+             ];
+
+            //  chama o método create do model User para criar o novo usuário no BD
+            User::create($data);
+            header('Location: index.php');
+        } else {
+            include 'views/register.php';
+        }
+    }
+}
+?>
