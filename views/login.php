102daa6182986bb4c31a2f386bf5b2206c115560
sarahozeto <127869612+sarahozeto@users.noreply.github.com>
2024-09-17 08:32:45 -0300

    elaboração de estrutura HTML

---
+++ views/login.php
@@ -0,0 +1,24 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>LLogin</title>
+</head>
+<body>
+    <main>
+        <form action="">
+            <section>
+                <label for="">Email:</label>
+                <input type="email" name="email" placheholder="email" required>
+            </section>
+            <section>
+                <label for="">Senha:</label>
+                <input type="password" name="senha" placheholder="*********" required>
+            </section>
+            <button type="submit">Login</button>
+        </form>
+        <a href="">Cadastre-se</a>
+    </main>
+</body>
+</html>
---
+++ views/register.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html lang="pt-br">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Cadastre-se</title>
+</head>
+<body>
+    <div>
+        <h2>Cadastro de usuário</h2>
+        <form method="post" action="">
+            <label for="">Nome:</label>
+            <input type="text" name="nome"  id="nome" required>
+            <label for="">E-mail:</label>
+            <input type="email" name="email" id="email">
+            <label for="">Senha:</label>
+            <input type="password" name="senha" id="senha" required>
+            <label for="">Perfil:</label>
+            <select name="perfil" id="perfil">
+                <option value="admin">ADMIN</option>
+                <option value="gestor">GESTOR</option>
+                <option value="colaborador">COLABORADOR</option>
+            </select>
+            <button type="submit">Cadastrar</button>
+        </form>
+        <a href="">Volter ao login</a>
+    </div>
+</body>
+</html>
