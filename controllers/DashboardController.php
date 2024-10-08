4362dbde40aa8874294a94be911bf15e713e2cfb
sarahozeto <127869612+sarahozeto@users.noreply.github.com>
2024-10-08 08:15:42 -0300

    inicia sessão para verificação de usuario

---
+++ controllers/DashboardController.php
@@ -0,0 +1,17 @@
+<?php
+
+class DashboardController
+{
+    // Inicia uma sessão para verificar se o usuário esta autenticado
+    public function index (){
+        session_start();
+
+        if(!isset($_SESSION['usuario_id'])){
+            header('Location: index');
+            exit; //garante que o script seja interrompido após o redirecionamento
+        }
+        include 'viwes/dashboard.php';
+    }
+}
+
+?>
