d86ba6741b2831ec50722955522b0a1f669011af
sarahozeto <127869612+sarahozeto@users.noreply.github.com>
2024-11-12 11:45:55 -0300

    inserção dos perfis e o php para identificar

--- views/edit_users.php
+++ views/edit_users.php
@@ -5,25 +5,25 @@
 <meta charset="UTF-8">
 <meta name="viewport" content="width=device-width, initial-scale=1.0">
 <title>Editar Usuário</title>
-<link rel="stylesheet" href="css/edit.css">
+<link rel="stylesheet" href="CSS/edit.css">
 </head>
  
 <body class="edit-body">
 <div class="edit-container">
-<h2>Editar Usuário</h2>
-<form method="post" action="" class="edit-form">
-<label for="nome">Nome:</label>
-<input type="text" name="nome" id="nome" value="" required>
+    <h2>Editar Usuário</h2>
+        <form method="post" action="index.php?action=edit&id=<?= $user['id']?>" class="edit-form">
+            <label for="nome">Nome:</label>
+            <input type="text" name="nome" id="nome" value="<?= $user['nome']?>" required>
  
             <label for="email">Email:</label>
-<input type="email" name="email" id="email" value="" required>
- 
-            <label for="perfil">Perfil:</label>
-<select name="perfil" id="perfil">
-<option value="">Admin</option>
-<option value="" >Gestor</option>
-<option value="" >Colaborador</option>
-</select>
+            <input type="email" name="email" id="email" value="<?= $user['email']?>" required>
+
+        <label for="perfil">Perfil:</label>
+    <select name="perfil" id="perfil">
+        <option value="admin" <?= $user['perfil'] == 'admin' ? 'selected' : ''?>>Admin</option>
+        <option value="gestor" <?= $user['perfil'] == 'gestor' ? 'selected' : ''?>>Gestor</option>
+        <option value="colaborador" <?= $user['perfil'] == 'colaborador' ? 'selected' : ''?>>Colaborador</option>
+    </select>
  
             <button type="submit" class="btn">Salvar</button>
 </form>
