1664a2ba0e5ebcb54db44c9cd193eca41119f779
sarahozeto <127869612+sarahozeto@users.noreply.github.com>
2024-11-13 07:47:05 -0300

    adicionando imagens

--- routes.php
+++ routes.php
@@ -1,27 +1,16 @@
 <?php
-//inclui arquivos de controlador nescessários para lidar com diferentes ações 
 require 'controllers/AuthController.php';
-//inclui o controlador de autenticação
 require 'controllers/UserController.php';
-//inclui o controlador de usuários
 require 'controllers/DashboardController.php';
-//inclui o controlador de dashboard
-//Cria instâncias dos controladores para utilizar seus métodos
 $authController = new AuthController();
-//instância controlador de auteticação
 $userController = new UserController();
-//instância controlador de usuário
 $dashboardController = new DashboardController();
-//instância controlador de dashboard
 
-//coleta a acão de URL, se não houver ação definida, usa 'login' como padrão
 $action = $_GET['action'] ?? 'login';
-//usa operador de coalescência nula (??) para definir 'login' se 'action' não estiver presente
 
 switch($action){
     case 'login':
         $authController->login();
-        //chama o método de login do controlador de autênticação
        break;
     case 'register':
         $userController->register();
